3abbf8768f7ac353a2a991339b80456c19f67d14
MARIO GONZALEZ GALINDO <mgzzgal@hotmail.com>
2022-04-30 15:23:36 -0500

    added test modules

--- src/biosig/eeg/simulations.py
+++ src/biosig/eeg/simulations.py
@@ -4,30 +4,31 @@ from numpy import array, ndarray, number, random, zeros
 from pandas import DataFrame
 from scipy.signal import find_peaks
 
+
 @dataclass(frozen=True)
-class NeuronType :
+class NeuronType:
     """The Izhikevich neuron types provided for each neuron"""
 
-    TONIC_SPIKING               : ClassVar[dict] = {"Name" : "Tonic Spiking",                   "a" : 0.02,     "b" : 0.2,  "c" : -65,  "d" : 6     }
-    PHASIC_SPIKING              : ClassVar[dict] = {"Name" : "Phasic Spiking",                  "a" : 0.02,     "b" : 0.25, "c" : -65,  "d" : 6     }
-    TONIC_BURSTING              : ClassVar[dict] = {"Name" : "Tonic Bursting",                  "a" : 0.02,     "b" : 0.2,  "c" : -50,  "d" : 2     }
-    PHASIC_BURSTING             : ClassVar[dict] = {"Name" : "Phasic Bursting",                 "a" : 0.02,     "b" : 0.2,  "c" : -55,  "d" : 0.05  }
-    MIXED_MODE                  : ClassVar[dict] = {"Name" : "Mixed Mode",                      "a" : 0.02,     "b" : 0.2,  "c" : -55,  "d" : 4     }
-    SPIKE_FREQUENCY_ADAPTATION  : ClassVar[dict] = {"Name" : "Spike Frequency Adaptation",      "a" : 0.01,     "b" : 0.2,  "c" : -65,  "d" : 8     }
-    CLASS_ONE_EXCITABILITY      : ClassVar[dict] = {"Name" : "Class One Excitability",          "a" : 0.02,     "b" : -0.1, "c" : -55,  "d" : 6     }
-    CLASS_TWO_EXCITABILITY      : ClassVar[dict] = {"Name" : "Class Two Excitability",          "a" : 0.2,      "b" : 0.26, "c" : -65,  "d" : 0     }
-    SPIKE_LATENCY               : ClassVar[dict] = {"Name" : "Spike Lateny",                    "a" : 0.02,     "b" : 0.2,  "c" : -65,  "d" : 6     }
-    SUBTHRESHOLD_OSCILATION     : ClassVar[dict] = {"Name" : "Subthreshold Oscilation",         "a" : 0.05,     "b" : 0.26, "c" : -60,  "d" : 0     }
-    RESONATOR                   : ClassVar[dict] = {"Name" : "Resonator",                       "a" : 0.1,      "b" : 0.26, "c" : -60,  "d" : -1    }
-    INTEGRATOR                  : ClassVar[dict] = {"Name" : "Integrator",                      "a" : 0.02,     "b" : -0.1, "c" : -55,  "d" : 6     }
-    REBOUND_SPIKE               : ClassVar[dict] = {"Name" : "Rebound Spike",                   "a" : 0.03,     "b" : 0.25, "c" : -60,  "d" : 4     }
-    REBOUND_BURST               : ClassVar[dict] = {"Name" : "Rebound Burst",                   "a" : 0.03,     "b" : 0.25, "c" : -52,  "d" : 0     }
-    THRESHOLD_VARIABILITY       : ClassVar[dict] = {"Name" : "Threshold Variability",           "a" : 0.03,     "b" : 0.25, "c" : -60,  "d" : 5     }
-    BISTABILITY                 : ClassVar[dict] = {"Name" : "Bistability",                     "a" : 0.1,      "b" : 0.26, "c" : -60,  "d" : 0     }
-    DAP                         : ClassVar[dict] = {"Name" : "DAP",                             "a" : 1,        "b" : 0.2,  "c" : -60,  "d" : -21   }
-    ACCOMODATION                : ClassVar[dict] = {"Name" : "Accomodation",                    "a" : 0.02,     "b" : 1,    "c" : -55,  "d" : 4     }
-    INHIBITION_INDUCED_SPIKING  : ClassVar[dict] = {"Name" : "Ihibition Induced Spiking",       "a" : -0.02,    "b" : -1,   "c" : -60,  "d" : 8     }
-    INHIBITION_INDUCED_BURSTING : ClassVar[dict] = {"Name" : "Inhibition Induced Bursting",     "a" : -0.026,   "b" : -1,   "c" : -45,  "d" : -2    }
+    TONIC_SPIKING: ClassVar[dict[str ]] = {"Name" : "Tonic Spiking",                   "a" : 0.02,     "b" : 0.2,  "c" : -65,  "d" : 6     }
+    PHASIC_SPIKING: ClassVar[dict] = {"Name" : "Phasic Spiking",                  "a" : 0.02,     "b" : 0.25, "c" : -65,  "d" : 6     }
+    TONIC_BURSTING: ClassVar[dict] = {"Name" : "Tonic Bursting",                  "a" : 0.02,     "b" : 0.2,  "c" : -50,  "d" : 2     }
+    PHASIC_BURSTING: ClassVar[dict] = {"Name" : "Phasic Bursting",                 "a" : 0.02,     "b" : 0.2,  "c" : -55,  "d" : 0.05  }
+    MIXED_MODE: ClassVar[dict] = {"Name" : "Mixed Mode",                      "a" : 0.02,     "b" : 0.2,  "c" : -55,  "d" : 4     }
+    SPIKE_FREQUENCY_ADAPTATION: ClassVar[dict] = {"Name" : "Spike Frequency Adaptation",      "a" : 0.01,     "b" : 0.2,  "c" : -65,  "d" : 8     }
+    CLASS_ONE_EXCITABILITY: ClassVar[dict] = {"Name" : "Class One Excitability",          "a" : 0.02,     "b" : -0.1, "c" : -55,  "d" : 6     }
+    CLASS_TWO_EXCITABILITY: ClassVar[dict] = {"Name" : "Class Two Excitability",          "a" : 0.2,      "b" : 0.26, "c" : -65,  "d" : 0     }
+    SPIKE_LATENCY: ClassVar[dict] = {"Name" : "Spike Lateny",                    "a" : 0.02,     "b" : 0.2,  "c" : -65,  "d" : 6     }
+    SUBTHRESHOLD_OSCILATION: ClassVar[dict] = {"Name" : "Subthreshold Oscilation",         "a" : 0.05,     "b" : 0.26, "c" : -60,  "d" : 0     }
+    RESONATOR: ClassVar[dict] = {"Name" : "Resonator",                       "a" : 0.1,      "b" : 0.26, "c" : -60,  "d" : -1    }
+    INTEGRATOR: ClassVar[dict] = {"Name" : "Integrator",                      "a" : 0.02,     "b" : -0.1, "c" : -55,  "d" : 6     }
+    REBOUND_SPIKE: ClassVar[dict] = {"Name" : "Rebound Spike",                   "a" : 0.03,     "b" : 0.25, "c" : -60,  "d" : 4     }
+    REBOUND_BURST: ClassVar[dict] = {"Name" : "Rebound Burst",                   "a" : 0.03,     "b" : 0.25, "c" : -52,  "d" : 0     }
+    THRESHOLD_VARIABILITY: ClassVar[dict] = {"Name" : "Threshold Variability",           "a" : 0.03,     "b" : 0.25, "c" : -60,  "d" : 5     }
+    BISTABILITY: ClassVar[dict] = {"Name" : "Bistability",                     "a" : 0.1,      "b" : 0.26, "c" : -60,  "d" : 0     }
+    DAP: ClassVar[dict] = {"Name" : "DAP",                             "a" : 1,        "b" : 0.2,  "c" : -60,  "d" : -21   }
+    ACCOMODATION: ClassVar[dict] = {"Name" : "Accomodation",                    "a" : 0.02,     "b" : 1,    "c" : -55,  "d" : 4     }
+    INHIBITION_INDUCED_SPIKING: ClassVar[dict] = {"Name" : "Ihibition Induced Spiking",       "a" : -0.02,    "b" : -1,   "c" : -60,  "d" : 8     }
+    INHIBITION_INDUCED_BURSTING: ClassVar[dict] = {"Name" : "Inhibition Induced Bursting",     "a" : -0.026,   "b" : -1,   "c" : -45,  "d" : -2    }
 
     @classmethod
     def get_by_name(cls, name : str) -> list :
@@ -61,7 +62,7 @@ class Neuron :
     _tau : ClassVar[float] = 0.025
 
     ## INIT ONLY VARIABLES
-    __type : InitVar[dict | str] = NeuronType.TONIC_SPIKING
+    __type : InitVar[dict | str] = field(NeuronType.TONIC_SPIKING)
 
     ## INSTANCE VARIABLES
     v0 : float | int = field(default=-60.)
@@ -192,42 +193,42 @@ class Network :
         """
 
         if weight_matrix : #check if there is an input weight matrix
-            if isinstance(weight_matrix, (DataFrame, ndarray)) and weight_matrix.shape != (self._total_neurons, self._total_neurons) :
+            if isinstance(weight_matrix, (DataFrame, ndarray)) and weight_matrix.shape != (self._total_neurons, self._total_neurons):
                 raise ValueError("Invalid input! The dimensions of the input weight_matrix must be square and equal to the total number of neurons of the network.")
-            if (isinstance(weight_matrix, ndarray) and weight_matrix.dtype != number) or (isinstance(weight_matrix, DataFrame) and weight_matrix.select_dtypes(exclude=['number']).any()) :
+            if (isinstance(weight_matrix, ndarray) and weight_matrix.dtype != number) or (isinstance(weight_matrix, DataFrame) and weight_matrix.select_dtypes(exclude=['number']).any()):
                 raise ValueError("Invalid datatype in matrix! The matrix must only contain numbers.")
             self._weights = weight_matrix
             return self._weights
 
-        if not labels : #check if a list of labels was provided
+        if not labels: #check if a list of labels was provided
             labels = [f"n_{x}" for x in range(self._total_neurons)] #generate a standard list of labels if none were provided
             self._labels = labels
-        elif self._total_neurons > len(labels) :
+        elif self._total_neurons > len(labels):
             raise ValueError("The list of labels must contain elements equal or greater than the number of neurons in the network!")
 
-        if not (0 < conn_rate < 1) :
+        if not (0 < conn_rate < 1):
             raise ValueError("The connection rate must be a positive number between 0 and 1!")
         
-        if 0 >= exc_cap or 0 >= inh_cap :
+        if 0 >= exc_cap or 0 >= inh_cap:
             raise ValueError("The excitation and inhibition caps must be positive numbers greater than 0!")
             
         conn_values = array([[(random.random() > (1 - conn_rate)) for _ in range(self._total_neurons)] for _ in range(self._total_neurons)])
         weight_values = zeros(conn_values.shape, dtype=float) #generate a square weight matrix
         
-        for j in range(weight_values.shape[0]) :
-            for i in range(weight_values.shape[1]) :
-                if conn_values[j, i] and labels[j] != labels[i] :
+        for j in range(weight_values.shape[0]):
+            for i in range(weight_values.shape[1]):
+                if conn_values[j, i] and labels[j] != labels[i]:
                     #assign a random weight value to the (j, i)_th cell of the matrix, representing the connection weight
                     #between the j_th neuron and the i_th neuron. positive if the ith neuron is excitatory, else, negative, capped by the network
                     weight_values[j, i] = (exc_cap if self.neurons[i].is_excitatory else -inh_cap) * random.random()
         self._weights = DataFrame(weight_values, index=labels, columns=labels)
         return self._weights
 
-    def print_weights(self) :
+    def print_weights(self):
         """Prints the weight matrix of the network to the console."""
         print(f"Weights and connection matrix:\n{self._weights.to_string()}")
 
-    def run_in_period(self, T : int, I_in : float = 0, trigger_pos : int = 0, trigger_duration : int = 200, trigger_cap : int | float= 1, trigger_type : NeuronType = NeuronType.Tonic_Spiking) -> tuple[ndarray, DataFrame, DataFrame] :
+    def run_in_period(self, T : int, I_in : float = 0, trigger_pos : int = 0, trigger_duration : int = 200, trigger_cap : int | float= 1, trigger_type : NeuronType = NeuronType.Tonic_Spiking) -> tuple[ndarray, DataFrame, DataFrame]:
         """
         Activates the network for a given amount of time evaluated in miliseconds using a trigger neuron response voltage
         
@@ -249,12 +250,12 @@ class Network :
         v_individual : dict[str, list[float]] = {n_label : list() for n_label in self._labels}
         v_field : list[float] = list() #field voltage response (sum of all neuron voltages)
 
-        #trigger parameters and responses
+        # trigger parameters and responses
         trigger_neuron : Neuron = Neuron(tau=Neuron._tau, is_excitatory=True)
         _, trigger_peaks = trigger_neuron.activate(T=trigger_duration, I_in=I_in)
         I_net[trigger_pos] = trigger_peaks.size * trigger_cap
 
-        for _ in range(int(T / Neuron._tau)) :
+        for _ in range(int(T / Neuron._tau)):
             v_field.append(sum(v))
             I_net = [self.excitation_input * random.randn() if n.is_excitatory else self.inhibition_input * random.randn() for n in self.neurons]
             fired = [30 <= v[idx] for idx, _ in enumerate(v)]
@@ -265,15 +266,15 @@ class Network :
                 neuron_voltage[label].append(fired[n_idx])
         return array(v_field), DataFrame(v_individual), DataFrame(neuron_voltage)
 
-    def __post_init__(self) -> None :
+    def __post_init__(self) -> None:
         if 0. >= self.excitation_input or 0. >= self.inhibition_input :
             raise ValueError("The excitation and inhibition inputs must have positive, greater than 0 values!")
         
         if 0 < len(self.neurons) :
             self._total_neurons = len(self.neurons)
             self.set_weight_matrix(labels=self._labels)
-    
+
     def __repr__(self) -> str:
-        message : str = f"""New Izhikevich network has been generated!
+        message: str = f"""New Izhikevich network has been generated!
         Total neurons: {self._total_neurons}"""
         return message
