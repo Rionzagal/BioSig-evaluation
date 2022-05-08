625c666a0f2224baba035322d300a2dc362d5a27
MARIO GONZALEZ GALINDO <mgzzgal@hotmail.com>
2022-05-07 21:07:50 -0500

    test completion and source correction

--- tests/test_izhikevich.py
+++ tests/test_izhikevich.py
@@ -1,5 +1,8 @@
-from biosig.eeg.izhikevich import Neuron, NeuronTypes
+from pandas import DataFrame
+from biosig.eeg.izhikevich import Network, Neuron, NeuronTypes
+from numpy import random
 import pytest
+
 
 @pytest.mark.parametrize("input_name,expected", [
     ("Tonic Spiking", NeuronTypes.TONIC_SPIKING),
@@ -82,4 +85,50 @@ def test_type_setting(test_type):
     test_neuron.set_neuron_type(test_type)
     type_name, _ = test_neuron.get_type()
     assert str(test_type["Name"]) == type_name
+    return
+
+
+@pytest.mark.parametrize("params, t_span, test_I, test_tau", [
+    ((-60, True, 0.5), 500, 10, 0.025),
+    ((-55, True, 1), 200, 5, 0.03),
+    ((-45, False, 0.25), 1000, 6, 0.01),
+    ((-26, False, 0.95), 800, 10, 0.05)
+])
+def test_neuron_run(params, t_span, test_I, test_tau):
+    v0, exc, awn = params
+    test_neuron: Neuron = Neuron(v0, exc, awn)
+    Neuron.set_tau(test_tau)
+    response, _ = test_neuron.activate(t_span, test_I)
+    assert int(t_span / Neuron._tau) == len(response)
+    return
+
+
+@pytest.mark.parametrize("quantity, exc_inh, conn_params, run_params", [
+    (10, (0.8, 1.22), (0.5, 1.2, 4.1), (100, 5, 1, 100, 4)),
+    (15, (0.05, 1.45), (0.3, 3.4, 1.6), (500, 14, 0, 50, 2.2)),
+    (45, (2.45, 0.22), (0.8, 1, 6), (200, 7, 20, 200, 1.5)),
+    (100, (0.65, 4.33), (0.1, 6.4, 2), (350, 11, 81, 500, 2)),
+    (5, (3, 0.5), (0.2, 5, 0.1), (1000, 0, 2, 300, 0.5))
+])
+def test_network(quantity, exc_inh, conn_params, run_params) -> None:
+    """Test designed to test the behavior of the Network model in the Izhikevich Module"""
+    test_neurons: list[Neuron] = [
+        Neuron(-70 * random.rand(), 0.5 > random.rand(), 0.5 + random.rand()) for _ in range(quantity)
+    ]
+    exc_input, inh_input = exc_inh
+    net: Network = Network(test_neurons, exc_input, inh_input)
+    net.print_weights()
+    assert str(quantity) in net.__repr__()
+    c_rate, e_cap, i_cap = conn_params
+    assert (quantity, quantity) == net.set_weights(c_rate, e_cap, i_cap).shape
+    labels: list[str] = [ f"neuron: {i}" for i in range(quantity) ]
+    custom_weights: DataFrame = DataFrame([
+        [ random.rand() if n.is_excitatory else -random.rand() for n in net.neurons ] for n in net.neurons
+        ])
+    custom_weights = net.set_existing_weights(custom_weights, labels)
+    assert (quantity, quantity) == custom_weights.shape
+    t_span, current, tr_pos, tr_duration, tr_cap = run_params
+    field_response, individual_response, activations = net.run(t_span, current, tr_pos, tr_duration, tr_cap)
+    assert int(t_span / Neuron._tau) == len(field_response)
+    assert (int(t_span / Neuron._tau), quantity) == individual_response.shape
     return
